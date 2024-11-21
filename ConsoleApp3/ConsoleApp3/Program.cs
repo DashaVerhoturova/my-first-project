0be1b88ca7e3dcdc791b14de11ca9edd11122d42
DashaVerhoturova <dverhoturova356@gmail.com>
2024-11-21 14:52:48 +0800

    try catch finally

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Введите целое число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Вы ввели число: {number}");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Ошибка: Пожалуйста, введите корректное целое число.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Произошла непредвиденная ошибка: {ex.Message}");
+        }
+        finally
+        {
+            Console.WriteLine("Завершение выполнения программы.");
+        }
+    }
+}
