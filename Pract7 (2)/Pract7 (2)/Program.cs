209ab8d460ba132cbfcb2c1ea50174547800b17b
DashaVerhoturova <dverhoturova356@gmail.com>
2024-11-26 22:06:45 +0800

    Pract7 (2)

---
+++ Pract7 (2)/Pract7 (2)/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите целое число: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int result = AbsoluteDifference(n);
+        Console.WriteLine($"Результат: {result}");
+    }
+
+    static int AbsoluteDifference(int n)
+    {
+        int difference = Math.Abs(n - 123);
+        if (n > 123)
+        {
+            return difference * 3;
+        }
+        return difference;
+    }
+}
